b39ffabd05f956d7ca4e1bd170561eee11ad09cf
ckharide <reachmouli@gmail.com>
2020-05-06 12:13:30 +0530

    Array Operations

--- array-decl.js
+++ array-decl.js
@@ -1,3 +1,15 @@
 let simpleArray = ['one', 2, 'three', true, false];
 console.log(simpleArray.length);
 
+// modify the array 
+
+function mixedNumbers(arr) {
+    // Only change code below this line
+  
+    // Only change code above this line
+    arr.unshift("I", "2",  "three")
+    arr.push("7", "VIII", "9")
+    return arr;
+  }
+  
+  console.log(mixedNumbers(['IV', 5, 'six']));
