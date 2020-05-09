d1cff5f4d9c5f89ade520a215fb65afe32d74123
ckharide <reachmouli@gmail.com>
2020-05-09 19:30:30 +0530

    has Own property adding and modifying new properties

--- array-decl.js
+++ array-decl.js
@@ -51,5 +51,94 @@ function mixedNumbers(arr) {
   }
   
   console.log(copyMachine([true, false, true], 2));
-  console.log(copyMachine([1, 2, 3], 5));
+ // console.log(copyMachine([1, 2, 3], 5));
 
+
+  function filteredArray(arr, elem) {
+    let newArr = [];
+    for (let i = 0; i < arr.length;i++) {
+       // console.log(arr[i]);
+        if(arr[i].indexOf(elem) == -1) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr;
+  }
+
+
+
+  console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+  console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18));
+  console.log(filteredArray([ ["amy", "beth", "sam"], ["dave", "sean", "peter"] ], "peter"));
+
+  // Accessign an element property via element[property ] or element.property
+
+  let users = {
+    Alan: {
+      age: 27,
+      online: true
+    },
+    /*Jeff: {
+      age: 32,
+      online: true
+    },*/
+    Sarah: {
+      age: 48,
+      online: true
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function isEveryoneHere(obj) {
+
+    return obj.hasOwnProperty('Sarah') && obj.hasOwnProperty('Alan') && obj.hasOwnProperty('Jeff')
+    
+  }
+
+  function countOnline(usersObj) {
+    let count = 0
+    for(let key in usersObj) {
+        if(usersObj[key]['online'] == true) {
+            count = count+1;
+        }
+    }
+   // console.log(count)
+    return count
+  }
+  
+  //temp = countOnline({ Alan: { online: false }, Jeff: { online: false }, Sarah: { online: false } });
+  //console.log(temp);
+
+  let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+      username: 'kennethCodesAllDay',
+      joinDate: 'March 26, 2016',
+      organization: 'freeCodeCamp',
+      friends: [
+        'Sam',
+        'Kira',
+        'Tomo'
+      ],
+      location: {
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'USA'
+      }
+    }
+  };
+  
+  function addFriend(userObj, friend) {
+    if (userObj.hasOwnProperty('data')) {
+        userObj['data']['friends'].push(friend);
+        return userObj['data']['friends']
+    }
+  }
+
+  console.log(addFriend(user, "Chandra"));
+  console.log(user);
+  
