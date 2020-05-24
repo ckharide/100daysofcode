98cf228adcb97377d2cd2f290942d5f6e2ef84e5
ckharide <reachmouli@gmail.com>
2020-05-24 11:14:21 +0530

    Exercices

--- inter-script.js
+++ inter-script.js
@@ -27,3 +27,58 @@ function sumVariant(arr) {
 }
   
 console.log(sumAll([4, 1])); // return 10
+
+
+function diffArray(arr1, arr2) {
+    var newArr = [];
+    var myArr = arr2.slice(0,arr2.length);
+    //return newArr;
+    for (let i =0; i < arr1.length; i++){
+       myArr = myArr.filter(x => x != arr1[i]);
+    }
+
+    newArr.push(...myArr);
+    myArr = arr1.slice(0,arr1.length);
+    for (let j =0; j < arr2.length; j++){
+        myArr = myArr.filter(x => x != arr2[j]);
+     }
+
+    newArr.push(...myArr);
+    return newArr;
+
+    /**** Another efficient way
+     * 
+     * function diffArray(arr1, arr2) {
+    return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+    function diff(a, b) {
+    return a.filter(item => b.indexOf(item) === -1);
+    }
+    ***/
+}
+
+function destroyer(arr) {
+    /*let remaining = [];
+    for(let k =1 ; k < arguments.length ; k ++){
+        var i = arguments[0].indexOf(arguments[k]);
+        arr.splice(i, 1);
+    }
+    return arr;*/
+
+    var args = Array.from(arguments).slice(1);
+    console.log(args);
+  return arr.filter(function(val) {
+    return !args.includes(val);
+  });
+}
+
+function filterout(arr1, arr2){
+    return arr1.filter(item => (arr2.indexOf(item) !== -1))
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+console.log("Destroyer " + destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+
+  
+  
