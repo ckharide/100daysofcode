b7564a912f7bba658e53c41515eaca21e9e2894d
ckharide <reachmouli@gmail.com>
2020-06-01 17:56:04 +0530

    new file algoproject

--- inter-script.js
+++ inter-script.js
@@ -363,6 +363,9 @@ var Person = function(firstAndLast) {
 
 
 function orbitalPeriod(arr) {
+  for (var elem in arr) {
+    console.log(arr[elem]);
+  }
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
   var a = 2 * Math.PI;
