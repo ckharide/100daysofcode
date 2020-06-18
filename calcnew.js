d4b1f7162c343ca221f0ab7880002a7624585a7e
ckharide <reachmouli@gmail.com>
2020-06-19 00:45:39 +0530

    fix parsing logic

--- calcnew.js
+++ calcnew.js
@@ -1,6 +1,7 @@
 var operatorSt = new Stack();
 var numberSt = new Stack();
 var opseen = false;
+var isPriorityOp = false;
 function showClock() {
 
 document.querySelector('.calculator-keys').addEventListener('click', (event) => {
@@ -9,13 +10,25 @@ document.querySelector('.calculator-keys').addEventListener('click', (event) => 
     return;
   }
 
+
   if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
-    updateRunningDisplay(target.value);
-    let result = updateOperatorStack(target.value);
-    console.log(result);
+    var opresult = target.value;
     opseen = true;
-    updateRunningDisplay(result);
+    if(opresult !== '='){
+        operatorSt.push(opresult);
+        if(operatorSt.size() >=1 && opresult != '*') {
+            let result = evaluteOperatorStack(opresult);
+            updateRunningDisplay(result);
+        }
+        updateRunningDisplay(numberSt.peek());
+    }   
+        
+    else 
+    {
+        console.log("Inside equal");
+        let result = evaluate(operatorSt, numberSt,opresult);
+        updateRunningDisplay(result);
+    }
     return;
   }
 
@@ -30,9 +43,15 @@ document.querySelector('.calculator-keys').addEventListener('click', (event) => 
     return;
   }
 
-  updateCurrentDisplay(target.value);
-  updateNumberStack(target.value);
-  console.log('digit', target.value);
+  addNumberStack(target.value);
+  updateRunningDisplay(target.value);
+  if(isPriorityOp === true){
+    isPriorityOp = false;
+    console.log("Inside priority Op");
+    opresult = evaluate(operatorSt, numberSt,opresult);
+    updateRunningDisplay(opresult);
+    return;
+  }
 });
   
 }
@@ -52,16 +71,19 @@ function updateRunningDisplay(value){
     document.querySelector('.calculator-screen').value = value;
 }
 
-function updateOperatorStack(op){
+function evaluteOperatorStack(op){
     var number = [];
     op = op.trim();
     if(op == '=') {
-        return evaluate(operatorSt, numberSt);
+        return evaluate(operatorSt, numberSt,op);
     }
     else if(op !== '=')
     {
         console.log("Pushing value" , op);
         operatorSt.push(op);
+        if(op === '*' || op === '/') {
+            isPriorityOp = true;
+        }
         while(!numberSt.isEmpty()) {
             number.unshift(numberSt.pop());
         }
@@ -74,39 +96,49 @@ function updateOperatorStack(op){
 
 }
 
-function updateNumberStack(val){
+function addNumberStack(val){
     numberSt.push(val);
 }
 
 function reset(){
     operatorSt = new Stack();
     numberSt = new Stack();
+    updateRunningDisplay(0);
+    opseen = true;
 }
 
-function evaluate(opstack , numstack) {
+function evaluate(opstack , numstack, opvalue) {
+    if(numstack.size() < 2) return opvalue;
     console.log("Operator value");
     var op1 =0, op2 = 0;
     let result = 0
-    while(!numstack.isEmpty() )
+   
+    while(!numstack.isEmpty() && !opstack.isEmpty())
     {
-        op1 = numberSt.pop();
-        op2 = numberSt.pop();
-    }
-    op1 = parseFloat(op1);
-    op2 = parseFloat(op2);
-    console.log(op2);
-    if(!opstack.isEmpty()) {
-        op = opstack.pop();
-        switch(op) {
-            case '+' : result = op1 + op2;
-                       break;
-            case '-' : result = op1 - op2;
-                        break;
-            case '*' : result = op1 * op2;
-                       break;
-            case '/' : result = op1 / op2;
-            break;
+        op1 = numstack.pop();
+        op2 = numstack.pop();
+        op1 = parseFloat(op1);
+        op2 = parseFloat(op2);
+        console.log("OP1 ==> " + op1 + "op2 ==> " + op2);
+        if(!opstack.isEmpty()) {
+            op = opstack.pop();
+            switch(op) {
+                case '+' : result = op1 + op2;
+                           break;
+                case '-' : result = op1 - op2;
+                            break;
+                case '*' : result = op1 * op2;
+                           break;
+                case '/' : result = op1 / op2;
+                break;
+            }
         }
+        result = parseFloat(result);
+        if(!numstack.isEmpty())
+            numstack.push(result);
+        else break;
     }
     return result;
+    
 }
+ 
